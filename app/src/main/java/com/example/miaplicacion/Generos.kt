a5a9ccb2d49f65abcc5966d41b3f534d8e02be5c
alu10194039 <aluurs4039@ieselcaminas.org>
2021-09-30 15:41:54 +0200

    TiLayout

--- app/src/main/java/com/example/miaplicacion/Generos.kt
+++ app/src/main/java/com/example/miaplicacion/Generos.kt
@@ -3,6 +3,8 @@ package com.example.miaplicacion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class Generos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,12 @@ class Generos : AppCompatActivity() {
 
         val bottomAppBar = findViewById(R.id.bottom_appbar) as BottomAppBar
         bottomAppBar.replaceMenu(R.menu.menu_main)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+        fab.setOnClickListener {
+            Snackbar.make(it, "Desplazando el boton" , Snackbar.LENGTH_SHORT).show()
+        }
 
     }
 }

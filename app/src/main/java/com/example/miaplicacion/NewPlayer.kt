79183065725fe76995c12531d19f281f6be129e7
alu10194039 <aluurs4039@ieselcaminas.org>
2021-10-07 17:12:29 +0200

    Tema 3

--- app/src/main/java/com/example/miaplicacion/NewPlayer.kt
+++ app/src/main/java/com/example/miaplicacion/NewPlayer.kt
@@ -2,7 +2,11 @@ package com.example.miaplicacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.view.get
 import  kotlinx.android.synthetic.main.activity_new_player.*
 
 class NewPlayer : AppCompatActivity() {
@@ -14,7 +18,15 @@ class NewPlayer : AppCompatActivity() {
         val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, telefono)
 
         spinner.adapter = adaptador
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-
+        spinner.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(applicationContext, parent!!.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+                editTextTextPersonName5.setText(spinner.selectedItem.toString())
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        })
     }
 }
